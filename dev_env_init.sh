b242384ed03cf5de35fd2870b7473e720eed887b
Sam Zamora <62625412+szmra@users.noreply.github.com>
2022-02-26 15:16:05 -0600

    Update dev_env_init.sh

--- dev_env_init.sh
+++ dev_env_init.sh
@@ -1,3 +1,4 @@
 #assuming fresh install
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install python
+#python3 installed at /opt/homebrew/bin/python3 (for big sur)
